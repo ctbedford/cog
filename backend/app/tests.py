394c90028951c196d372b309e79d2b4cff5eff96
ctbedford <tybed7@icloud.com>
2023-11-25 22:46:15 -0600

    test

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Task
+
+class TaskModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Task.objects.create(title='first task', description='a test task', completed=False)
+
+    def test_title_label(self):
+        task = Task.objects.get(id=1)
+        field_label = task._meta.get_field('title').verbose_name
+        self.assertEquals(field_label, 'title')
+
+    def test_completed_default(self):
+        task = Task.objects.get(id=1)
+        self.assertFalse(task.completed)
