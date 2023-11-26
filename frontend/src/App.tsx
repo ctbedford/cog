2cfd73f05f7b9e8fdf286dce8d35ec23d1d9905e
ctbedford <tybed7@icloud.com>
2023-11-26 03:09:12 -0600

    Changed env vars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h1>Welcome to the React App</h1>
+        <p>This is a test</p>
       </header>
     </div>
   );
