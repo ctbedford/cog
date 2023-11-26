34dce2a957e28ab851270987ee85d2e70b899c31
ctbedford <tybed7@icloud.com>
2023-11-25 20:31:32 -0600

    Added git CI CD

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('tasks/', views.task_list, name='task-list'),
+    path('tasks/', views.task_list, name='task-list'),
 ]
